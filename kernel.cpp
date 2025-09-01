7c0e78e09f2dacf67c82cc16f7c968ad056a82ca
Harun Adiyaman <haruna@vt.edu>
2025-09-01 12:01:24 -0400

    Added call constructors & types.h
    - Call constructors will call the constructos for static/global objects

--- kernel.cpp
+++ kernel.cpp
@@ -1,12 +1,22 @@
+#include "types.h"
 
-void printf(char* string){
-    unsigned short* vga_buffer = (unsigned short*) 0xb8000;
+void printf(int8_t* string){
+    uint16_t* vga_buffer = (uint16_t*) 0xb8000;
     for(int i=0; string[i] != '\0'; ++i){
         vga_buffer[i] = (vga_buffer[i] & 0xFF00) | string[i];
     }
 }
 
-extern "C" void pingu_kernel_main(void* multiboot_struct, unsigned int magic_number){
+typedef void (*constructor)(); //function pointer
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void call_constructors(){
+    for(constructor* i=&start_ctors; i!=&end_ctors; ++i){
+        (*i)();
+    }
+}
+
+extern "C" void pingu_kernel_main(void* multiboot_struct, uint32_t magic_number){
     printf("Noot Noot!");
 
     while(1);
